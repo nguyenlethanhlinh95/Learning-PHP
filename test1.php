2b6cc3850febdb32d31a8447dd8feecc78103fea
Thanh Linh <nguyenlethanhlinh95@gmail.com>
2019-01-11 22:22:23 +0700

    First commit for example

---
+++ test1.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Computer of Linh
+ * Date: 1/10/2019
+ * Time: 11:55 PM
+ */
+
+class test1
+{
+    protected $_name;
+
+    /**
+     * test1 constructor.
+     */
+
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->_name = $name;
+    }
+
+
+}
+
